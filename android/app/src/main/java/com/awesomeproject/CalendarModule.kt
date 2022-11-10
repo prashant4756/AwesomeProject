4dee39c58b21114ebcddf9550627e34c2d32ea6e
Prashant Jha <prashant4756@gmail.com>
2022-11-10 12:16:11 +0530

    get constant

--- android/app/src/main/java/com/awesomeproject/CalendarModule.kt
+++ android/app/src/main/java/com/awesomeproject/CalendarModule.kt
@@ -1,6 +1,7 @@
 package com.awesomeproject
 
 import android.util.Log
+import android.widget.Toast
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
@@ -13,6 +14,10 @@ class CalendarModule(reactContext: ReactApplicationContext): ReactContextBaseJav
     @ReactMethod
     fun createCalendarEvent(eventName:String, location: String) {
         Log.d("CalendarModule", "Kotlin Create event called with name: $eventName and location: $location")
+        Toast.makeText(reactApplicationContext, "Kotlin Create event called with name: $eventName and location: $location", Toast.LENGTH_SHORT).show()
     }
 
+    override fun getConstants(): MutableMap<String, String> {
+        return mutableMapOf<String, String>(Pair("eventName1","1"), Pair("eventName2","2"))
+    }
 }
--- src/screen/CalendarScreen.js
+++ src/screen/CalendarScreen.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {Button, NativeModules} from 'react-native';
+import {Button} from 'react-native';
+import CalendarModule from '../androidmodule/CalendarModuleInterface';
+// import CalendarModule from '../androidmodule/CalendarModule';
 
 const CalendarScreen = ({navigation}) => {
-  const {CalendarModule} = NativeModules;
   const onPress = () => {
     console.log('We will invoke the native module here!');
     CalendarModule.createCalendarEvent('testName', 'testLocation');
   };
+  const onPressGetConstants = () => {
+    console.log('We will get constants here!');
+    // const {eventName1} = CalendarModule.getConstants();
+    console.log(CalendarModule.getConstants());
+  };
   return (
     <>
       <Button title="invoke calendar" onPress={onPress} />
+      <Button title="getConstants calendar" onPress={onPressGetConstants} />
     </>
   );
 };
